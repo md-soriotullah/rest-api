c64afe1c50eb89e1a89e0f2396ea8608c17d4ca0
mdsoriotullah <soriothabib@gmail.com>
2022-09-27 18:45:14 +0600

    first commit api

---
+++ server.js
@@ -0,0 +1,30 @@
+const express = require('express');
+const dotenv = require('dotenv').config();
+const colors = require('colors');
+
+
+// init environment veriable
+const PORT = process.env.PORT || 8080;
+
+// express init
+const app = express();
+
+// express middlewares
+app.use(express.json());
+app.use(express.urlencoded( { extended : false } ));
+
+// listen port
+app.listen(PORT, () => {
+    console.log(`server is running on port ${PORT}`.bgGreen.black);
+});
+
+
+
+
+
+// module exports
+// module.exports = {
+//     dotenv,
+//     express,
+//     colors
+// }
